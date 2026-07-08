7c44ddbada7410c2734a341f2a974bd505e73812
Seyyit Battal Arvas <seyyitbattalarvas3@gmail.com>
2026-07-08 08:59:31 +0300

    new project created

---
+++ limits-service/src/main/java/com/in28minutes/microservices/limits_service/controller/LimitsController.java
@@ -0,0 +1,15 @@
+package com.in28minutes.microservices.limits_service.controller;
+
+import com.in28minutes.microservices.limits_service.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+    return  new Limits(1,1000);
+    }
+
+}
