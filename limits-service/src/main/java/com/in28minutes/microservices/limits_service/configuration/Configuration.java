439ab93b1364b0d21734279a940c8a441aeecdea
Seyyit Battal Arvas <seyyitbattalarvas3@gmail.com>
2026-07-13 11:05:20 +0300

    Config properties connected on application properties

---
+++ limits-service/src/main/java/com/in28minutes/microservices/limits_service/configuration/Configuration.java
@@ -0,0 +1,17 @@
+package com.in28minutes.microservices.limits_service.configuration;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ConfigurationProperties("limits-service")
+public class Configuration {
+    private int minimum;
+    private int maximum;
+}
