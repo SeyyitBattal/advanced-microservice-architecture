630c37443efea0fe7268f887516804c04ad27233
Seyyit Battal Arvas <seyyitbattalarvas3@gmail.com>
2026-08-05 13:42:31 +0300

    h2 console added

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchange.java
@@ -1,5 +1,8 @@
 package com.in28minutes.microservices.currency_exchange_service;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,13 +10,17 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+@Entity
 @Component
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CurrencyExchange {
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
